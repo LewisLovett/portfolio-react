e5e07c00e21363aec311d2059462524133438467
Lewis Lovett <llovett7575@gmail.com>
2022-11-30 15:56:52 +0000

    Create dfile structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Navbar/>
+      <Welcome/>
+      <Projects/>
+      <Skills/>
+      <Experience/>
+      <About/>
+      <Contact/>
+    </main>
+  );
+}
+
+export default App;
